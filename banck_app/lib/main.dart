363d39b1a8275ea40d89a878af6dc6095d2d7d3a
Chamikara Mihiranga Jayasinghe <128024648+cmjayasinghe@users.noreply.github.com>
2026-01-29 00:41:46 +0530

    Customized login screen

--- banck_app/lib/main.dart
+++ banck_app/lib/main.dart
@@ -11,15 +11,52 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'NSB Bank',
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
+      home: const LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFF0B4D78),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text(
+                    'WELCOME!',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Icon(
+                    Icons.account_balance,
+                    color: Colors.white,
+                    size: 100,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        body: const Center(child: Text('Welcome to NSB Bank App')),
       ),
     );
   }
