39ef99aea80beb8078dcb8aa2d5158abf9de3e0e
UCHITHA SAHAN <uchithasahan1235@gmail.com>
2026-01-28 12:25:58 +0530

    Appbarchange

---
+++ banck_app/android/app/src/main/kotlin/com/example/banck_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.banck_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ banck_app/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'NSB Bank',
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center(child: Text('Welcome to NSB Bank App')),
+      ),
+    );
+  }
+}
