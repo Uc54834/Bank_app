1a28015dfd6e1e80c6b91a936e9120c25876158a
Chamikara Mihiranga Jayasinghe <128024648+cmjayasinghe@users.noreply.github.com>
2026-01-30 01:38:49 +0530

    Update the Sign Up button

--- banck_app/lib/pages/login_page.dart
+++ banck_app/lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'signup_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -102,7 +103,12 @@ class LoginPage extends StatelessWidget {
             // Sign Up link (NO direct import)
             TextButton(
               onPressed: () {
-                // Navigation will be added after 
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignupPage(),
+                  ),
+                );
               },
               child: const Text('New to Bank Apps? Sign Up'),
             ),
